d2d8bb0c8b13ce789474c053f2cb1d440d0c264a
yuhuan <jyuhuan@gmail.com>
2015-08-19 21:05:21 -0400

    fix regex error

--- src/main/scala/me/yuhuan/util/Regex.scala
+++ src/main/scala/me/yuhuan/util/Regex.scala
@@ -4,11 +4,11 @@ package me.yuhuan.util
  * @author Yuhuan Jiang (jyuhuan@gmail.com).
  */
 object Regex {
-  val whitespaces = """[ \t]+""" r
-  val whitespace = """\s""" r
-  val leftParenthesis = """\(""" r
-  val rightParenthesis = """\)""" r
-  val allBrackets = """(\()|(\))""" r
+  val whitespaces = """[ \t]+"""
+  val whitespace = """\s"""
+  val leftParenthesis = """\("""
+  val rightParenthesis = """\)"""
+  val allBrackets = """(\()|(\))"""
 
-  val betweenTags = """>[^<>]+</""" r
+  val betweenTags = """>[^<>]+</"""
 }
